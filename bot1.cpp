5be3e10bc34be11fd5559253e1565848dbeb05cc
slxshxr17 <szczepaniakdominik5@gmail.com>
2022-12-29 21:51:19 +0100

    Poprawienie funkcji trafienie przez uwzglednienie statkow o ksztalcie L

--- bot1.cpp
+++ bot1.cpp
@@ -31,6 +31,9 @@ class Bot1{
         }
     }
     string strzal(){
+        while(!strzelic[kolejka_strzalow.front().first][kolejka_strzalow.front().second]){
+            kolejka_strzalow.pop();
+        }
         if(!kolejka_strzalow.empty()){
             pair<int, int> para_z_kolejki = kolejka_strzalow.front();
             string miejsce_strzalu = wspolrzedneNaString(para_z_kolejki.first, para_z_kolejki.second);
@@ -47,33 +50,50 @@ class Bot1{
         strzelic[wiersz][kolumna] = false;
         return pole_strzal;
         //TODO: zapytanie o informacje o trafieniu po strzale i wywolanie funkcji decyzja
+        // pomysl rozwiazania: 
+        // decyzja(werdykt(strzal())) gdzie werdykt to jakas funkcja gry ktora przyjmuje miejsce gdzie strzelic i zwraca miejsce strzalu i informacje pudlo/zatopiony/trafiony, a decyzja bierze te trzy informacje i podejmuje decyzje bota na ich podstawie
     }
     void decyzja(int kolumna, int wiersz){ //funkcja decydujaca co robic na podstawie czy bot trafil czy nie
         bool trafienie = false; 
         bool zatopienie = false;
         //dwie linijki wyzej maja byc na podstawie informacji do funkcji gry, obecnie sa tylko w celu testu
+        
+        // zalozmy ze mamy czesc takiej planszy:
+        // ...#...
+        // ...#...
+        // ...#...
+        // niech kolumna i wiersz wskazuja na hash srodkowy i zalozmy ze w hash najwyzej juz trafilismy w jakims strzale wczesniej
+        // sprawdzmy wiec najpierw czy dookola [kolumna-65][wiersz] jest jakis punkt ktory byl trafiony
+        // jesli byl trafiony (jest to u nas punkt nad nami), to do kolejki chcemy dodac tylko punkt pod nami, bo punkt nad nami jest juz trafiony, a punkty na lewo i prawo na pewno nie beda miec statku, bo mialby wtedy ksztalt L 
         if(trafienie){
             bool czy_wczesniej_trafione_obok = false;
             for(auto para : kierunki){
                 int x = kolumna-65+para.second;
                 int y = wiersz+para.first;
-                // zalozmy ze y = 3 x = 4
-                // kierunek: -1 0
-                // czyli na y=2 x = 4 bylo trafienie
-                // chcemy dodac y=1 x = 4 i y=4 x = 4, bo nie wiemy jak dalej idzie statek
-                // jesli kierunek 1 0 to chcemy dodac to samo, wiec mamy y+para.first i y - 2*para.first, jesli w planszy
-                // mozemy smiało dodac do obu poniewaz jedna ze zmiennych w kierunku jest 0 wiec nie wplynie na druga zmienna, a zalatwimy tym oba przypadki
                 if(!poza_plansza(y, x) && trafiony[y][x]){
                     czy_wczesniej_trafione_obok = true;
-                    if(!poza_plansza(y+para.first, x+para.second)){
-                        kolejka_strzalow.push({y+para.first, x+para.second});
+                    if(!poza_plansza(y-2*para.first, x-2*para.second) && strzelic[y-2*para.first][x-2*para.second]){
+                        kolejka_strzalow.push({y-2*para.first, x-2*para.second}); //nie musimy rozszerzać w drugą stronę, bo mamy na pewno tamten punkt dodany przez punkt trafiony obok
                     }
-                    if(!poza_plansza(y-2*para.first, x-2*para.second)){
-                        kolejka_strzalow.push({y-2*para.first, x-2*para.second});
+                    for(auto para2 : kierunki){
+                        if(para.first != 0){
+                            if(para2.first == 0){ //naszym celem jest usuniecie statkow ktore stworza L (skoro byl trafiony np. punkt pod nami, to na pewno nie chcemy strzelic w punkt ktory jest po prawej i lewej od tego pod nami, bo wtedy statek by mial ksztalt L). wiec jesli para.first != 0 to znaczy ze poszlo y w gore o 1 lub dol, wiec nasza para2.first musi byc rowna 0 zeby nie isc y gora dol tylko x.
+                            // jesli dodamy tylko strzelic = false, to przy funkcji strzal() wyjmie nam z kolejki ten punkt jesli jest falszywy (1szy if w funkcji strzal())
+                                strzelic[y][x+para2.second] = false;
+                                strzelic[wiersz][kolumna-65+para2.second] = false; //zaznaczmy od razu kolo obecnego trafienia
+                            }       
+                        }
+                        else{
+                            if(para2.first != 0){
+                                strzelic[y+para2.first][x] = false;
+                                strzelic[wiersz+para2.first][kolumna-65] = false; //zaznaczmy od razu kolo obecnego trafienia
+                            }
+                        }
                     }
+                    break; //wiemy ze punkt np nad nami byl juz trafiony, wiec jedyny punkt ktory musimy dodac to ten pod nami ktory juz dodalismy wiec mozemy przerwac petle
                 }
             }
-            if(!czy_wczesniej_trafione_obok){
+            if(!czy_wczesniej_trafione_obok){ //jesli w nic dookola nie trafilismy i teraz trafilismy to statek moze kontynuoowac w dowolna strone
                 for(auto para : kierunki){
                     if(!poza_plansza(wiersz+para.second, kolumna-65+para.first)){
                         kolejka_strzalow.push({wiersz+para.second, kolumna-65+para.first});
