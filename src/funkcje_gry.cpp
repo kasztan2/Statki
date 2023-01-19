81e82d6a13e9bd3a084333620c585be663891967
Krzysiek <338171@uwr.edu.pl>
2023-01-19 23:19:40 +0100

    poprawki bota, wyświetlania, prawie działa

--- src/funkcje_gry.cpp
+++ src/funkcje_gry.cpp
@@ -49,11 +49,11 @@ int koniec_gry(int ilosc_statkow1[4], int ilosc_statkow2[4]){
 }
 
 int strzal_w_pole(int kolumna, int wiersz, char plansza[][10], bool poprzednie_strzaly[][10], int ilosc_statkow[4]){
-    if(kolumna>60)kolumna-=65;
+    if(kolumna>60)kolumna-=0;
     if(plansza[kolumna][wiersz] == '#'){
         if(plansza==plansza2)poprzednie_udane_strzaly1[kolumna][wiersz]=true;
         queue<pair<int, int>> q;
-        q.push({kolumna+65, wiersz});
+        q.push({kolumna+0, wiersz});
         bool visited[10][10];
         memset(visited, 0, sizeof(visited));
         int byl_jakis_obok_trafiony = 0;
@@ -68,13 +68,13 @@ int strzal_w_pole(int kolumna, int wiersz, char plansza[][10], bool poprzednie_s
                     byl_jakis_obok_trafiony++;
                 }
                 if(plansza[kol+u.first][wier+u.second] == '#' && !poprzednie_strzaly[kol+u.first][wier+u.second]){
-                    poprzednie_strzaly[kolumna+65][wiersz] = true;
+                    poprzednie_strzaly[kolumna+0][wiersz] = true;
                     return 1; // jest trafienie, a obok stoi dalej jakis statek ktory nie zostal zatopiony
                 }
             }
         }
         ilosc_statkow[byl_jakis_obok_trafiony+1]--;
-        poprzednie_strzaly[kolumna+65][wiersz] = true;
+        poprzednie_strzaly[kolumna+0][wiersz] = true;
         return 2;
 
     }
