e91ec4eb8a35b5aec15cbb9d8dfe51e775999e22
root <root@DESKTOP-CBE9BCR.localdomain>
2023-01-24 01:53:20 +0100

    przywrócenie poprzedniej wersji bota i poprawki widoku planszy

--- src/bot3.h
+++ src/bot3.h
@@ -14,71 +14,71 @@ using std::string, std::cout, std::deque, std::to_string, std::pair, std::vector
 
 class Bot3{
     private:
-    
-    int tablicawar [10][10]; //tablica wartości strzałów dla bota
 
-    bool isemptyshot (int do_rysowania [10][10]){
+    int tablicawar [10][10];
+
+    bool isemptyshot (int tablica [10][10]){
 for (int i=0;i<10;i++){
     for (int j =0;j<10;j++){
-       if (do_rysowania [i][j]==1 || do_rysowania [i][j]==2)
+       if (tablica [i][j]== 1 || tablica [i][j]!=2)
         return false;
     }}
     return true;
 }
-  bool is4 (int do_rysowania [10][10]){
+  bool is4(char tablica_bot[10][10]){
 for (int i = 0; i < 10; i++) {
     for (int j = 0; j < 7; j++) {
-            if (do_rysowania[i][j] == 1 && do_rysowania[i][j+1] == 1 && do_rysowania[i][j+2] == 1 && do_rysowania[i][j+3] == 1) {
-                return false;
+            if (tablica_bot[i][j] == '#' && tablica_bot[i][j+1] == '#' && tablica_bot[i][j+2] == '#' && tablica_bot[i][j+3] == '#') {
+                return true;
             }
         }
     }
     for (int i = 0; i < 7; i++) {
         for (int j = 0; j < 10; j++) {
-            if (do_rysowania[i][j] == 1 && do_rysowania[i+1][j] == 1 && do_rysowania[i+2][j] == 1 && do_rysowania[i+3][j] == 1) {
-                return false;
+            if (tablica_bot[i][j] == '#' && tablica_bot[i+1][j] == '#' && tablica_bot[i+2][j] == '#' && tablica_bot[i+3][j] == '#') {
+                return true;
             }
         }
     }
-    return true;
+    return false;
 }  
 
-bool is3(int do_rysowania [10][10]){
+bool is3(char tablica_bot [10][10]){
 
 for (int i = 0; i < 10; i++) {
     for (int j = 0; j < 8; j++) {
-            if (do_rysowania[i][j] == 1 && do_rysowania[i][j+1] == 1 && do_rysowania[i][j+2] == 1 ) {
-                return false;
+            if (tablica_bot[i][j] == '#' && tablica_bot[i][j+1] == '#' && tablica_bot[i][j+2] == '#' ) {
+                return true;
             }
         }
     }
     for (int i = 0; i < 8; i++) {
         for (int j = 0; j < 10; j++) {
-            if (do_rysowania[i][j] == 1 && do_rysowania[i+1][j] == 1 && do_rysowania[i+2][j] == 1 ) {
-                return false;
+            if (tablica_bot[i][j] == '#' && tablica_bot[i+1][j] == '#' && tablica_bot[i+2][j] == '#' ) {
+                return true;
             }
         }
     }
-    return true;
+    return false;
 }
 
-bool is2(int do_rysowania [10][10]){
+bool is2(char tablica_bot [10][10]){
 
 for (int i = 0; i < 10; i++) {
     for (int j = 0; j < 9; j++) {
-            if (do_rysowania[i][j] == 1 && do_rysowania[i][j+1] == 1 ) {
-                return false;
+            if (tablica_bot[i][j] == '#' && tablica_bot[i][j+1] == '#' ) {
+                return true;
             }
         }
     }
     for (int i = 0; i < 9; i++) {
         for (int j = 0; j < 10; j++) {
-            if (do_rysowania[i][j] == 1 && do_rysowania[i+1][j] == 1) {
-                return false;
+            if (tablica_bot[i][j] == '#' && tablica_bot[i+1][j] == '#') {
+                return true;
             }
         }
     }
-    return true;
+    return false;
 }
 
 
@@ -90,15 +90,15 @@ bool is1(char tablica_bot[10][10],int do_rysowania[10][10]){
         }
     }
     return false;
-} // funkcja sprawdzająca czy plansza ma statki
+}
 
     
    int bot (char tablica_bot [10][10],int tablicawar[10][10],int do_rysowania [10][10]){
 int miejscestrzal;
-int z1 = 0;
-int z2 =0;
+int z1;
+int z2;
 if (isemptyshot(do_rysowania)){
-debug("a ");
+
 
 srand(time(NULL));
 miejscestrzal = rand()%8;
@@ -120,12 +120,12 @@ for (int i=0;i<10;i++){
 return (x*10+y);
 }
 
-else if (is4(do_rysowania)){
-debug("b ");
+else if (is4(tablica_bot)){
+
   for (int i=0;i<10;i++){
     for (int j =0;j<10;j++){
        if (do_rysowania [i][j]== 1 ||do_rysowania [i][j]== 2 ){
-        tablicawar [i][j] = -1;
+        tablicawar [i][j] = -1;}
         z1= (10*i+3+j)/10;
         z2= (10*i+3+j)%10;
         if (tablicawar[z1][z2]!=-1 && 10*i+3+j<100){
@@ -146,41 +146,40 @@ debug("b ");
         if (tablicawar[z1][z2]!=-1 && 10*(i+3)+j<100){
             tablicawar[z1][z2] = tablicawar [z1][z2] +1;
         }
-    }}}
-   
+    }}
+
   int maximum =0;
   int ile = 0;
     for (int i=0;i<10;i++){
     for (int j =0;j<10;j++){
-       if (tablicawar [i][j]>maximum)
+       if (tablicawar [i][j]>=maximum)
         maximum = tablicawar [i][j];}}
 
     for (int i=0;i<10;i++){
     for (int j =0;j<10;j++){
        if (tablicawar [i][j]==maximum)
         ile++;}}
-        
 
    srand(time(NULL));
 miejscestrzal = rand()%ile;
 ile =0;
 
-
 for (int i=0;i<10;i++){
     for (int j =0;j<10;j++){
-       if (tablicawar [i][j] ==maximum && ile<=miejscestrzal){
+       if (tablicawar [i][j]==maximum && ile<=miejscestrzal){
         z1 = i;
         z2 = j;
         ile++;
        }
     }
-}
+}   
 
+debug("aaaaaaaaaaaa");
+tablicawar [z1][z2] = -1;
    return (z1*10+z2);
 }
 
-else if(is3(do_rysowania)){
-     debug("c ");
+else if(is3(tablica_bot)){
   for (int i=0;i<10;i++){
     for (int j =0;j<10;j++){
        if (do_rysowania [i][j]== 1 ||tablica_bot [i][j]== 2)
@@ -211,7 +210,7 @@ else if(is3(do_rysowania)){
   int ile = 0;
     for (int i=0;i<10;i++){
     for (int j =0;j<10;j++){
-       if (tablicawar [i][j]>maximum)
+       if (tablicawar [i][j]>=maximum)
         maximum = tablicawar [i][j];}}
 
     for (int i=0;i<10;i++){
@@ -234,8 +233,7 @@ for (int i=0;i<10;i++){
 }
    return (z1*10+z2);
 }
-else if (is2(do_rysowania)){
-    debug("d ");
+else if (is2(tablica_bot)){
    for (int i=0;i<10;i++){
     for (int j =0;j<10;j++){
        if (do_rysowania [i][j]== 1||do_rysowania [i][j]== 2)
@@ -266,7 +264,7 @@ else if (is2(do_rysowania)){
   int ile = 0;
     for (int i=0;i<10;i++){
     for (int j =0;j<10;j++){
-       if (tablicawar [i][j]>maximum)
+       if (tablicawar [i][j]>=maximum)
         maximum = tablicawar [i][j];}}
 
     for (int i=0;i<10;i++){
@@ -290,21 +288,17 @@ for (int i=0;i<10;i++){
    return (z1*10+z2);
 }
 else {
-    debug("e ");
-int maximum =-100;
+int maximum =0;
   int ile = 0;
-
+    for (int i=0;i<10;i++){
+    for (int j =0;j<10;j++){
+       if (tablicawar [i][j]>=maximum)
+        maximum = tablicawar [i][j];}}
 
     for (int i=0;i<10;i++){
     for (int j =0;j<10;j++){
-       if (tablicawar [i][j] >=maximum){
-        maximum = tablicawar [i][j];}
-        }}
-
-    for (int i=0;i<10;i++){
-    for (int j =0;j<10;j++){
-       if (tablicawar [i][j]==maximum){
-        ile++;}}}
+       if (tablicawar [i][j]==maximum)
+        ile++;}}
 
    srand(time(NULL));
 miejscestrzal = rand()%ile;
@@ -312,18 +306,13 @@ ile =0;
 
 for (int i=0;i<10;i++){
     for (int j =0;j<10;j++){
-       if (tablicawar [i][j]==maximum && ile<miejscestrzal){
+       if (tablicawar [i][j]==maximum && ile<=miejscestrzal){
         z1 = i;
         z2 = j;
         ile++;
        }
     }
 }
-
-       
-debug(to_string(z1)+ " "+ to_string(z2));
- tablicawar [z1][z2] = -1;
-
    return (z1*10+z2);
 }
 }
@@ -331,27 +320,26 @@ debug(to_string(z1)+ " "+ to_string(z2));
     public:
     
     
-  Bot3() {
-    for (int i=0;i<10;i++){
+  Bot3() {for (int i=0;i<10;i++){
     for (int j =0;j<10;j++){
-       tablicawar [i][j]=0; // inicjalizacja tablicy wartości strzałów bota na zera
+       tablicawar [i][j]=0;
     }
 }
-tablicawar [1][1] = 1; // jedno z początkowych miejsc do strzału bota 
-tablicawar [2][0] = 1;
+tablicawar [1][1] = 1;
+//tablicawar [2][0] = 1;
 tablicawar [3][2] = 1;
-tablicawar [4][7] = 1;
+//tablicawar [4][7] = 1;
 tablicawar [5][9] = 1;
-tablicawar [6][8] = 1;
+//tablicawar [6][8] = 1;
 tablicawar [8][3] = 1;
-tablicawar [9][5] = 1;  
+//tablicawar [9][5] = 1;  
 }
     
     
-int shot_bot (char tablica_bot [10][10], bool poprzednie_strzaly[][10], int ilosc_statkow[4], int do_rysowania[10][10]){
+  int shot_bot (char tablica_bot [10][10], bool poprzednie_strzaly[][10], int ilosc_statkow[4], int do_rysowania[10][10]){
 int x;
 int y;
-int dostan = bot(tablica_bot,tablicawar,do_rysowania); // otrzymane miejsca do strzału
+int dostan = bot(tablica_bot,tablicawar,do_rysowania);
 
 x = dostan/10;
 y = dostan%10;
@@ -361,105 +349,39 @@ if (tablica_bot [x][y]== '#'){
     do_rysowania[x][y] = 1;
 
     if (tablica_bot[x+1][y] != '#'&& tablica_bot[x-1][y]!= '#' && tablica_bot[x][y+1] != '#' && tablica_bot[x][y-1]!= '#'){
-        if (x+1<10){do_rysowania [x+1][y] = 2; tablicawar [x+1][y] = -1;  } //dodaje tutaj zmiane tablicy wartosci
-        if (x-1 >=0){do_rysowania [x-1][y] = 2; tablicawar [x-1][y] = -1;}
-        if (y+1<10){do_rysowania [x][y+1] = 2; tablicawar [x][y+1] = -1;}
-        if (y-1 >=0){do_rysowania [x][y-1] =2; tablicawar [x][y-1] = -1;} 
-        if((x+1)<10 && (y+1)<10){do_rysowania [x+1][y+1] = 2; tablicawar [x+1][y+1] = -1;}
-        if((x-1)>=0 && (y-1)>=0){do_rysowania [x-1][y-1] = 2; tablicawar [x-1][y-1] = -1;}
-        if((x-1)>=0 && (y+1)<10){do_rysowania [x-1][y+1] = 2; tablicawar [x-1][y+1] = -1;}
-        if((x+1)<10 && (y-1)>=0){do_rysowania [x+1][y-1] = 2; tablicawar [x+1][y-1] = -1;} //to trzeba usunąć - bot ma tego nie rysowac 
+        if (x+1<10){tablicawar [x+1][y] = -1;}
+        if (x-1 >=0){tablicawar [x-1][y] = -1;}
+        if (y+1<10){tablicawar [x][y+1] = -1;}
+        if (y-1 >=0){tablicawar [x][y-1] =-1;}
+        if((x+1)<10 && (y+1)<10){
+        tablicawar [x+1][y+1] = -1;}
+
+      if((x-1)>0 && (y-1)>0){
+        tablicawar [x-1][y-1] = -1;}
+
+       if((x-1)>0 && (y+1)<10){
+        tablicawar [x-1][y+1] = -1;}
+
+              if((x+1)<10 && (y-1)>0){
+        tablicawar [x+1][y-1] = -1;}
 }
 
+  else {   if((x+1)<10 && (y+1)<10){
+        tablicawar [x+1][y+1] = -1;}
 
-    else if (do_rysowania[x-1][y]==1 || do_rysowania[x][y-1]==1 || do_rysowania[x][y+1]==1 || do_rysowania[x-1][y+1] ==1){ 
+      if((x-1)>0 && (y-1)>0){
+        tablicawar[x-1][y-1] = -1;}
 
-            if (do_rysowania[x-1][y]==1 && x-1 >=0){
-              if (do_rysowania[x-2][y]==1 && x-2 >=0){
-                if (do_rysowania[x-3][y]==1 && x-3 >=0){ 
-                     if (x-4 >=0){do_rysowania [x-4][y] = 2; tablicawar [x-4][y] = -1;}
-                     }
-                else { if (x-3 >=0){do_rysowania [x-3][y] = 2; tablicawar [x-3][y] = -1;}}
-            }
-             else { if (x-2 >=0){do_rysowania [x-2][y] = 2; tablicawar [x-2][y] = -1;}}
-            }
-        
-            if (do_rysowania[x][y+1]==1 && y+1 <10){
-              if (do_rysowania[x][y+2]==1 && y+2 <10){
-                if (do_rysowania[x][y+3]==1 && y+3 <10){ 
-                     if (y+4 <10){do_rysowania [x][y+4] = 2; tablicawar [x][y+4] = -1;}
-                     }
-                else { if (y+3 <10 ){do_rysowania [x][y+3] = 2; tablicawar [x][y+3] = -1;}}
-            }
-             else { if (y+2 <10){do_rysowania [x][y+2] = 2; tablicawar [x][y+2] = -1;}}
-            }
+       if((x-1)>0 && (y+1)<10){
+        tablicawar [x-1][y+1] = -1;}
 
-
-            if (do_rysowania[x+1][y]==1 && x+1 <10){
-              if (do_rysowania[x+2][y]==1 && x+2 <10){
-                if (do_rysowania[x+3][y]==1 && x+3 <10){ 
-                     if (x+4 <10){do_rysowania [x+4][y] = 2; tablicawar [x+4][y] = -1;}
-                     }
-                else { if (x+3 <10){do_rysowania [x+3][y] = 2; tablicawar [x+3][y] = -1;}}
-            }
-             else { if (x+2 <10){do_rysowania [x+2][y] = 2; tablicawar [x+2][y] = -1;}}
-            }
-
-        
-            if (do_rysowania[x][y-1]==1 && y-1 >=0){
-              if (do_rysowania[x][y-2]==1 && y-2 >=0){
-                if (do_rysowania[x][y-3]==1 && y-3 >=10){ 
-                     if (y-4 >=0){do_rysowania [x][y-4] = 2; tablicawar [x][y-4] = -1;}
-                     }
-                else { if (y-3 >=0 ){do_rysowania [x][y-3] = 2; tablicawar [x][y-3] = -1;}}
-            }
-             else { if (y-2 >=10){do_rysowania [x][y-2] = 2; tablicawar [x][y-2] = -1;}}
-            }
-     
-        if((x+1)<10 && (y+1)<10){do_rysowania [x+1][y+1] = 2; tablicawar [x+1][y+1] = -1;}
-        if((x-1)>=0 && (y-1)>=0){do_rysowania [x-1][y-1] = 2; tablicawar [x-1][y-1] = -1;}
-        if((x-1)>=0 && (y+1)<10){do_rysowania [x-1][y+1] = 2; tablicawar [x-1][y+1] = -1;}
-        if((x+1)<10 && (y-1)>=0){do_rysowania [x+1][y-1] = 2; tablicawar [x+1][y-1] = -1;} 
-
-
-
-
-        if (tablicawar[x+1][y] != -1){
-          if ((do_rysowania[x-2][y] == 1 && do_rysowania[x-3][y] == 1) || (do_rysowania[x-2][y] == 1 && tablica_bot[x-3][y] != '#')|| tablica_bot[x-2][y] != '#'){
-            tablicawar[x+1][y] = -1;
-          } }
-            
-         if (tablicawar[x-1][y] != -1){
-          if ((do_rysowania[x+2][y] == 1 && do_rysowania[x+3][y] == 1) || (do_rysowania[x+2][y] == 1 && tablica_bot[x+3][y] != '#')|| tablica_bot[x+2][y] != '#'){
-            tablicawar[x-1][y] = -1;
-          } }
-
-         if (tablicawar[x][y+1] != -1){
-          if ((do_rysowania[x][y-2] == 1 && do_rysowania[x][y-3] == 1) || (do_rysowania[x][y-2] == 1 && tablica_bot[x][y-3] != '#')|| tablica_bot[x][y-2] != '#'){
-            tablicawar[x][y+1] = -1;
-          } }
-
-        if (tablicawar[x][y-1] != -1){
-          if ((do_rysowania[x][y+2] == 1 && do_rysowania[x][y+3] == 1) || (do_rysowania[x][y+2] == 1 && tablica_bot[x][y+3] != '#')|| tablica_bot[x][y+2] != '#'){
-            tablicawar[x][y-1] = -1;
-          } }
-
-    }  //sytuacja kiedy statek jest zatopiony + rysowanie jest do usunięcia
-
-
-    else {   
-
-        if((x+1)<10 && (y+1)<10){do_rysowania [x+1][y+1] = 2; tablicawar [x+1][y+1] = -1;}
-        if((x-1)>=0 && (y-1)>=0){do_rysowania [x-1][y-1] = 2; tablicawar [x-1][y-1] = -1;}
-        if((x-1)>=0 && (y+1)<10){do_rysowania [x-1][y+1] = 2; tablicawar [x-1][y+1] = -1;}
-        if((x+1)<10 && (y-1)>=0){do_rysowania [x+1][y-1] = 2; tablicawar [x+1][y-1] = -1;} //to trzeba usunąć - bot ma tego nie rysowac 
-
+              if((x+1)<10 && (y-1)>0){
+        do_rysowania [x+1][y-1] = 2;}
 
         if (x+1<10 && tablicawar [x+1][y]!=-1){tablicawar [x+1][y] =(tablicawar [x+1][y]+1)*2;}
         if (x-1 >=0 && tablicawar [x-1][y]!= -1){tablicawar [x-1][y] =(tablicawar [x-1][y]+1)*2;}
         if (y+1<10 && tablicawar [x][y+1]!= -1){tablicawar [x][y+1] =(tablicawar [x][y+1]+1)*2;}
-        if (y-1 >=0 && tablicawar [x][y-1]!= -1){tablicawar [x][y-1] =(tablicawar [x][y-1]+1)*2;} //zwiększenie wartości pól do strzału wokół niezatopionego statku
-
+        if (y-1 >=0 && tablicawar [x][y-1]!= -1){tablicawar [x][y-1] =(tablicawar [x][y-1]+1)*2;}
         }
 
 
@@ -469,20 +391,19 @@ if (is1(tablica_bot,do_rysowania) ==false){
     ilosc_statkow [0] = 0;
     ilosc_statkow [1] = 0;
     ilosc_statkow [2] = 0;
-    ilosc_statkow [3] = 0; //koniec gry dla bota gdy plansza jest pusta
+    ilosc_statkow [3] = 0;
 }
 
 
 
-return 1; // bot trafił więc powtarza turę
+return 1;
 }
 
 
-do_rysowania[x][y] = 2; 
-return 0; //bot nie trafił
+do_rysowania[x][y] = 2;
+return 0;
 }
-
-return -1; // tutaj nigdy nie dojdzie bot jeżeli kod działa prawidłowo
+return -1;
 }
   
           
